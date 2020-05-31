87bb3e92788bc74c497c66e712ce60be08fd9637
rmarticedeno <rmarticedeno@gmail.com>
2020-05-30 20:30:24 -0400

    Added Constants file with proxy key identifiers

---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,13 @@
+class Constants {
+  Constants._();
+
+  static const String prefProxy = 'proxy';
+  static const String prefProxyCredentials = 'proxy-authorization';
+  static const String prefProxyHost = 'proxy-host';
+  static const String prefProxyPort = 'proxy-port';
+  static const String prefProxyCredentialsName = 'proxy-credentials-name';
+  static const String prefProxyCredentialsPassword =
+      'proxi-credentials-password';
+  static const String prefProxyAllowInsecureCertificates =
+      'proxy-insecure-certificates';
+}
